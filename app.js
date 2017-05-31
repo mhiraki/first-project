d7946d763dc7cfcee016b582274da47a0bef7d55
mhiraki <mayahiraki@gmail.com>
2017-05-30 17:44:42 -1000

    code refactor

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
-var numChildren = Math.floor(Math.random()*10);
-var partnerName = "Nick";
-var geographicLocation = "San Francisco";
-var jobTitle = "Architect";
+// //The Fortune Teller
+// var numChildren = Math.floor(Math.random()*10);
+// var partnerName = ["Tyler", "Steve", "Josh", "James", "Nick"];
+// var randoName = partnerName[Math.floor(Math.random() * partnerName.length)];
+// var geographicLocation = ["San Francisco", "New York City", "Los Angeles"];
+// var randoGeographic = geographicLocation[Math.floor(Math.random()*geographicLocation.length)];
+// var jobTitle = ["Architect", "CEO", "dog walker", "professor"];
+// var randoJob = jobTitle[Math.floor(Math.random()*jobTitle.length)];
 
-var fortune = "You will be a" + " " + jobTitle + " " + "in" + " " + geographicLocation + " " + "and married to" + " " + partnerName + " " +"with" + " " + numChildren + " " + "kids."
-console.log(fortune);
+// var fortune = "You will be a" + " " + randoJob + " " + "in" + " " + randoGeographic + " " + "and married to" + " " + randoName + " " +"with" + " " + numChildren + " " + "kids."
+// console.log(fortune);
 
-function ageCalc(born, future) {
-	var age = "You are" + " " + (future - born) + " " + "years old."
-	console.log(age)
+
+//The Fortune Teller function
+
+window.onload = function() {
+
+function fortuneTeller() {
+	var numChildren = Math.floor(Math.random()*10);
+	var partnerName = ["Tyler", "Steve", "Josh", "James", "Nick"];
+	var randoName = partnerName[Math.floor(Math.random() * partnerName.length)];
+	var geographicLocation = ["San Francisco", "New York City", "Los Angeles"];
+	var randoGeographic = geographicLocation[Math.floor(Math.random()*geographicLocation.length)];
+	var jobTitle = ["Architect", "CEO", "dog walker", "professor"];
+	var randoJob = jobTitle[Math.floor(Math.random()*jobTitle.length)];
+	var fortune = "You will be a " + randoJob +  " in " + randoGeographic + " and married to " + randoName + " with " + numChildren + " kids."
+	
+	return fortune;
 }
-ageCalc(1998,2017);
+	document.getElementById('test').innerHTML = fortuneTeller();
+}
+
+
+
+
+
+// //Age Calculator
+// function ageCalc(born, future) {
+// 	var age = "You are" + " " + (future - born) + " " + "years old."
+// 	console.log(age)
+// }
+
+
+// ageCalc(1998,2017);
+// fortuneTeller();
